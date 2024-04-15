30cd5aa97d023d0f24a60e903961eb385b055651
Lokeshch420 <lokeshcha2002@gmail.com>
2024-04-15 11:47:11 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 //require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+
+connectDB(process.env.PORT,()=>{
+  console.log(`Server listening at ${process.env.PORT}`)
+})
+.then(()=>{
+  app.listen()
+})
+.catch((err)=>{
+  console.log("DB connection failed" , err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // IIFI METHOD APPROACH
 // import  express  from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+         Promise.resolve(requestHandler(req,res,next)).catch((err)=>{next(err)})
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+//with try catch 
+// const asyncHandler = (fn)=>async(req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+//     }catch(err){
+//         res.status(500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
